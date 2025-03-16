aea92d4e883d3d0a0b667336725e30852eea9dea
Stefan Horning <stefanhorning02704@gmail.com>
2025-03-16 19:16:29 +0100

    homepage & Edit Search

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -13,5 +13,13 @@ import { AuthComponent } from "../auth/auth.component";
   imports: [CommonModule, HeaderComponent, SearchComponent, CarGalleryComponent, PopupEditSearchComponent]
 })
 export class HomepageComponent {
+  popupVisible: boolean = false;
 
+  openPopup() {
+    this.popupVisible = true;  // Pop-up anzeigen
+  }
+
+  closePopup() {
+    this.popupVisible = false;  // Pop-up schließen
+  }
 }
--- src/app/homepage/popup-edit-search/popup-edit-search.component.ts
+++ src/app/homepage/popup-edit-search/popup-edit-search.component.ts
@@ -1,25 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { FormsModule } from '@angular/forms';
 import { Options } from '@angular-slider/ngx-slider';
 import { NgxSliderModule } from '@angular-slider/ngx-slider';
 import { CommonModule } from '@angular/common';
+import { SearchComponent } from '../search/search.component';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-popup-edit-search',
-  imports: [MatSliderModule,FormsModule,NgxSliderModule,CommonModule],
+  imports: [MatSliderModule,FormsModule,NgxSliderModule,CommonModule,SearchComponent,ButtonComponent],
   templateUrl: './popup-edit-search.component.html',
   styleUrl: './popup-edit-search.component.scss'
 })
 export class PopupEditSearchComponent {
   minValue: number = 0;
-  maxValue: number = 20000;
+  maxValue: number = 50000;
+  @Output() close = new EventEmitter<void>();
   sliderOptions: Options = {
     floor: 0,
-    ceil: 20000,
+    ceil: 50000,
     step: 100,
     translate: (value: number): string => {
       return value.toLocaleString() + ' €';
     }
   };
+
+  today: string;
+  minEndDate: string;
+  startDate: string = ''; 
+  endDate: string = ''; 
+  cost: number = 0;
+
+  constructor() {
+    const todayDate = new Date();
+    this.today = todayDate.toISOString().split('T')[0];
+    this.minEndDate = this.today;
+  }
+
+  onStartDateChange(event: Event) {
+    this.startDate = (event.target as HTMLInputElement).value;
+    
+    // minEndDate einen Tag nach startDate setzen
+    const start = new Date(this.startDate);
+    start.setDate(start.getDate() + 1); // Einen Tag hinzufügen
+    this.minEndDate = start.toISOString().split('T')[0];
+  
+    // Falls das Enddatum vor dem neuen minEndDate liegt, setze es zurück
+    if (this.endDate && this.endDate < this.minEndDate) {
+      this.endDate = ''; // End-Date zurücksetzen
+    }
+  }
+
+  onClose() {
+    this.close.emit();
+  }
+
+  onEndDateChange(event: Event) {
+    this.endDate = (event.target as HTMLInputElement).value;
+  }
+
+  onPopUpClick(event: MouseEvent) {
+    event.stopPropagation();
+  }
 }
